9e03f655c8aad9fa2890a688e923cc712ab9d08a
Marc Baumgartner <marc@mabaka.ch>
2025-11-29 21:11:23 +0100

    Build a zip as deliverable

---
+++ release.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+rm -rf ./target
+
+# Step 1: Prepare the release (updates the version and creates a tag)
+mvn release:prepare
+
+# Step 2: Check out the release tag
+RELEASE_TAG=$(git describe --tags --abbrev=0)
+git checkout $RELEASE_TAG
+
+# Step 3: Build the Docker image tar with the release version
+mvn clean verify jib:buildTar -Dmaven.test.skip=true
+
+# Step 4: Switch back to the main branch and perform the release
+git checkout main
+mvn release:perform
