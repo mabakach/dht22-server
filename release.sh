9558ed22772a63c71139f91b719863fd63a8bc59
Marc Baumgartner <marc@mabaka.ch>
2025-11-30 15:57:36 +0100

    Remove unnecessary steps

--- release.sh
+++ release.sh
@@ -4,14 +4,5 @@ rm -rf ./target
 
 # Step 1: Prepare the release (updates the version and creates a tag)
 mvn release:prepare
-
-# Step 2: Check out the release tag
-RELEASE_TAG=$(git describe --tags --abbrev=0)
-git checkout $RELEASE_TAG
-
-# Step 3: Build the Docker image tar with the release version
-mvn clean verify jib:buildTar -Dmaven.test.skip=true
-
-# Step 4: Switch back to the main branch and perform the release
-git checkout main
+# Step 2: Perform the release (builds and deploys the release)
 mvn release:perform
